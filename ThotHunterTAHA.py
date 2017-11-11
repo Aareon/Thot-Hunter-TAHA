2abced4c7128c2a90ee949e8c2320a6c2c841179
Noor <noorhayatywadam@gmail.com>
2017-11-10 18:29:01 -0800

    Commited

---
+++ ThotHunterTAHA.py
@@ -0,0 +1,61 @@
+import random
+
+class Player:
+    player_name_opts = ('Jerry', 'Seinfield', 'Potato')
+    def __init__(self, **kwargs):
+        self.name = kwargs.get("name") or random.choice(self.player_name_opts)
+        self.level = kwargs.get("level") or 0
+        self.health = self.level * 3
+        self.attack_s = kwargs.get("attack") or self.level * 2
+
+    def __repr__(self):
+        return "<player(name={}, health={}, attack={}, level={})>".format(
+                self.level, self.pname, self.health, self.attack)
+
+    def __str__(self):
+        return "Level {} {} [HEALTH: {} | ATK: {} | Level = {}]".format(
+                self.level, self.name, self.health, self.attack)
+
+    def attack():
+        if turn == 1:
+            choice = input('Type 1 or 2 in order to either attack or skip the turn')
+        elif choice == '2':
+            turn = 0
+            return turn
+        elif choice == '1':
+            roll = random.randint(1, 100)
+            if roll == 69:
+                print('You missed!')
+                turn = 0
+            elif not roll == 69:
+                print('You Hit!')
+                Enemy.health = Enemy.health - Player.attack_s
+
+class Enemy:
+    name_opts = ("Thot", "Alpha Thot", "Thot with Pumkin Spice")
+    def __init__(self, **kwargs):
+        self.name = kwargs.get("name") or random.choice(self.name_opts)
+        self.health = kwargs.get("health") or random.randint(5, 50)
+        self.attack = kwargs.get("attack") or random.randint(1, 10)
+        self.experience = kwargs.get("experience") or random.randint(5,50)
+        level = kwargs.get("level")
+        if level is not None:
+            level = random.randint(level - 2, level + 2)
+        self.level = level
+
+    def __repr__(self):
+        return "<Enemy(name={}, health={}, attack={}, defense={}, level={}) xp={}>".format(
+                self.name, self.health, self.attack, self.defense, self.level, self.experience)
+
+    def __str__(self):
+        return "Level {} {} [HEALTH: {} | ATK: {} | XP: {}]".format(
+                self.level, self.name, self.health, self.attack, self.experience)
+
+def enemy_encounter(location_level):
+    enemy = Enemy(level=location_level)
+    return enemy
+
+
+if __name__ == "__main__":
+    print(enemy_encounter(5))
+Player.attack()
