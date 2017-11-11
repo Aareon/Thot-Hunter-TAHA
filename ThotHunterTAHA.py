b147bfcc1dec86ebcf5c4ddcaf0dfb2b2c5fe9e2
Noor <noorhayatywadam@gmail.com>
2017-11-10 19:21:26 -0800

    Added more names

--- ThotHunterTAHA.py
+++ ThotHunterTAHA.py
@@ -1,7 +1,7 @@
 import random
 
 class Player:
-    player_name_opts = ('Jerry', 'Seinfield', 'Potato')
+    player_name_opts = ('Jerry', 'Seinfield', 'Gumball The SJW Destroyer', 'Inigo The Brave', 'Noor The Thot/Furry/SJW Hunter', 'Erika Costell The Snake', 'Keemstar The Thot Obliterator')
     def __init__(self, **kwargs):
         self.name = kwargs.get("name") or random.choice(self.player_name_opts)
         self.level = kwargs.get("level") or 0
@@ -17,6 +17,10 @@ class Player:
                 self.level, self.name, self.health, self.attack)
 
     def attack():
+        try:
+            choice
+        except NameError:
+            choice
         try:
             turn
         except NameError:
@@ -35,7 +39,7 @@ class Player:
                 Enemy.health = Enemy.health - Player.attack_s
 
 class Enemy:
-    name_opts = ("Thot", "Alpha Thot", "Thot with Pumkin Spice")
+    name_opts = ("Thot", "Alpha Thot", "Thot with Pumkin Spice", 'Erika Costell')
     def __init__(self, **kwargs):
         self.name = kwargs.get("name") or random.choice(self.name_opts)
         self.health = kwargs.get("health") or random.randint(5, 50)
@@ -53,6 +57,19 @@ class Enemy:
     def __str__(self):
         return "Level {} {} [HEALTH: {} | ATK: {} | XP: {}]".format(
                 self.level, self.name, self.health, self.attack, self.experience)
+    def attack():
+        try:
+            turn
+        except NameError:
+            turn = 1
+            roll = random.randint(1, 100)
+            if roll == 69:
+                print('The enemy missed missed!')
+                turn = 1
+            elif not roll == 69:
+                print('The enemy Hit!')
+                Enemy.health = Player.health - Enemy.attack
+                print(f'you lost {enemy.attack} health!')
 
 def enemy_encounter(location_level):
     enemy = Enemy(level=location_level)
